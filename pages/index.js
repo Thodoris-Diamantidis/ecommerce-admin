f1ce1c6f6f987e4642664762d4e1ec05fdc9d8d7
Thodoris-Diamantidis <thodoris_diamantidis@hotmail.com>
2023-06-03 16:00:26 +0300

    created admin Nav

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,21 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Nav from "@/components/Nav"
+import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Home() {
+  const { data: session } = useSession()
+  if (!session){
+    return (
+      <div className="bg-blue-800 w-screen h-screen flex items-center">
+        <div className='text-center w-full'>
+          <button onClick={()=> signIn('google')} className='bg-white p-2 px-4 rounded-lg'>Login with Google</button>
+        </div>
+    </div>
+    )
+  }
   return (
-    <div className="bg-blue-800 w-screen h-screen flex items-center">
-      <div className='text-center w-full'>
-        <button className='bg-white p-2 px-4 rounded-lg'>Login with Google</button>
-      </div>
+    <div className="bg-blue-800 min-h-screen flex">
+      <Nav/>
+      <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4">Logged in {session.user.email}</div>
     </div>
 
   )
