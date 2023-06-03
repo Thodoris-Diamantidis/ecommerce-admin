d78fe125f11551e79aaff472e0acad460b914b96
Thodoris-Diamantidis <thodoris_diamantidis@hotmail.com>
2023-06-03 20:39:55 +0300

    added axios and created add product page

--- pages/products/new.js
+++ pages/products/new.js
@@ -1,10 +1,44 @@
 import Layout from "@/components/Layout";
+import { useState } from "react";
 
 export default function NewProduct(){
+    const [title,setTitle] = useState('')
+    const [description,setDescription] = useState('')
+    const [price,setPrice] = useState('')
+
+    function createProcut(){
+
+    }
     return (
         <Layout>
-            <input type="text" placeholder="product name"/>
-            <textarea placeholder="description"></textarea>
+            <form onSubmit={createProcut}>
+            <h1>New Product</h1>
+
+            <label>Product name</label>
+            <input 
+                type="text"
+                placeholder="product name" 
+                value={title}
+                onChange={ev => setTitle(ev.target.value)}/>
+
+            <label>Description</label>
+            <textarea
+                placeholder="description"
+                value={description}
+                onChange={ev => setDescription(ev.target.value)}/>
+
+            <label>Price (in EURO)</label>
+            <input
+                type="number"
+                placeholder="price"
+                value={price}
+                onChange={ev => setPrice(ev.target.value)}/>
+            <button 
+                type='submit'
+                className="btn-primary">
+                Save
+            </button>
+            </form>
         </Layout>
     )
 }
