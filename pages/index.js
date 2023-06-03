d588e4ccec2330538118ae6a18f035c42bfd74be
Thodoris-Diamantidis <thodoris_diamantidis@hotmail.com>
2023-06-03 16:57:18 +0300

    add new product

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   const {data: session} = useSession()
   return (
   <Layout>
-    <div className="text-blue-800 flex justify-between">
+    <div className="text-blue-900 flex justify-between">
       <h2>
         Hello, <b>{session?.user?.name}</b>
       </h2>
