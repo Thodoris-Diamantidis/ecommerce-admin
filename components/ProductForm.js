10f28769662bb4c72579ee92d2fc8d8afc5f9a24
Thodoris-Diamantidis <thodoris_diamantidis@hotmail.com>
2023-06-10 15:24:57 +0300

    added spinner and installed sortablejs

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useRouter } from "next/router";
 import { useState } from "react";
+import Spinner from "./Spinner";
 
 export default function ProductForm({
     _id,
@@ -14,6 +15,7 @@ export default function ProductForm({
     const [price,setPrice] = useState(existingPrice || '')
     const [images,setImages] = useState(existingImages || [])
     const [goToProduct,setGoToProduct] = useState(false)
+    const [isUploading,setIsUploading] = useState(false)
     const router = useRouter()
 
     async function saveProduct(ev){
@@ -34,6 +36,7 @@ export default function ProductForm({
     async function uploadImages(ev) {
         const files = ev.target?.files
         if (files?.length > 0 ){
+            setIsUploading(true)
             const data = new FormData()
             for (const file of files){
                 data.append('file', file)
@@ -42,6 +45,7 @@ export default function ProductForm({
             setImages(oldImages => {
                 return [...oldImages, ...res.data.links]
             })
+            setIsUploading(false)
         }
     }
 
@@ -57,12 +61,17 @@ export default function ProductForm({
             <label>
                 Photos
             </label>
-            <div className="mb-2 flex flex-wrap gap-2">
+            <div className="mb-2 flex flex-wrap gap-1">
                 {!!images?.length && images.map(link => (
                     <div key={link} className="h-24">
                         <img src={link} alt="" className="rounded-lg"/>
                     </div>
                 ))}
+                {isUploading && (
+                    <div className="h-24 flex items-center">
+                        <Spinner />
+                    </div>
+                )}
                 <label className="cursor-pointer w-24 h-24 flex text-center items-center justify-center text-sm gap-1 text-gray-600 rounded-lg bg-gray-200">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
@@ -72,9 +81,6 @@ export default function ProductForm({
                     </div>
                     <input type="file"  onChange={uploadImages} className="hidden"/>
                 </label>
-                {!images?.length && (
-                <div>No photos in this product</div>
-                    )}
             </div>
             <label>Description</label>
             <textarea
---
+++ components/Spinner.js
@@ -0,0 +1,7 @@
+import { GridLoader } from "react-spinners";
+
+export default function Spinner(){
+    return (
+        <GridLoader size={10} color={"#86d636"} speedMultiplier={2}/>
+    )
+}
