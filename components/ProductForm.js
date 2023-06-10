52147d6d9a8ac2cdbbb9b0914ceb22a177141ffd
Thodoris-Diamantidis <thodoris_diamantidis@hotmail.com>
2023-06-10 15:10:54 +0300

    new functionality add images to products done

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -7,17 +7,18 @@ export default function ProductForm({
     title:existingTitle,
     description:existingDescription,
     price:existingPrice,
-    images,
+    images: existingImages,
 }){
     const [title,setTitle] = useState(existingTitle || '')
     const [description,setDescription] = useState(existingDescription || '')
     const [price,setPrice] = useState(existingPrice || '')
+    const [images,setImages] = useState(existingImages || [])
     const [goToProduct,setGoToProduct] = useState(false)
     const router = useRouter()
 
     async function saveProduct(ev){
         ev.preventDefault();
-        const data = {title,description,price}
+        const data = {title,description,price,images}
         if(_id){
             await axios.put('/api/products', {...data,_id})
         }else{
@@ -37,11 +38,10 @@ export default function ProductForm({
             for (const file of files){
                 data.append('file', file)
             }
-            const res = await fetch('/api/upload', {
-                method: 'POST',
-                body: data,
+            const res = await axios.post('/api/upload', data)
+            setImages(oldImages => {
+                return [...oldImages, ...res.data.links]
             })
-            console.log(res)
         }
     }
 
@@ -57,7 +57,12 @@ export default function ProductForm({
             <label>
                 Photos
             </label>
-            <div className="mb-2">
+            <div className="mb-2 flex flex-wrap gap-2">
+                {!!images?.length && images.map(link => (
+                    <div key={link} className="h-24">
+                        <img src={link} alt="" className="rounded-lg"/>
+                    </div>
+                ))}
                 <label className="cursor-pointer w-24 h-24 flex text-center items-center justify-center text-sm gap-1 text-gray-600 rounded-lg bg-gray-200">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,4 +1,9 @@
 import multiparty from 'multiparty'
+import {PutObjectCommand, S3Client} from '@aws-sdk/client-s3'
+import fs from 'fs'
+import mime from 'mime-types'
+
+const bucketName = 'theo-next-ecommerce'
 
 export default async function handle(req, res){
     const form = new multiparty.Form()
@@ -9,7 +14,31 @@ export default async function handle(req, res){
         })
     })
     console.log(files.file.length)
-    return res.json('ok')
+    
+    //create a client
+    const client = new S3Client({
+        region: 'eu-central-1',
+        credentials: {
+            accessKeyId: process.env.S3_ACCESS_KEY,
+            secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+        }
+    })
+    const links = []
+    for (const file of files.file){
+        const extension = file.originalFilename.split('.').pop()
+        const newFileName = Date.now() + "." + extension
+        console.log({extension,file,newFileName})
+        await client.send(new PutObjectCommand({
+            Bucket: bucketName,
+            Key: newFileName,
+            Body: fs.readFileSync(file.path),
+            ACL: 'public-read',
+            ContentType: mime.lookup(file.path),
+        }))
+        const link = `https://${bucketName}.s3.amazonaws.com/${newFileName}`
+        links.push(link)
+    }
+    return res.json({links})
 }
 
  export const config = {
